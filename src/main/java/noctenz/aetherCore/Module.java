46b41c5fca51aef315324c5290fb93d29dfacc68
sovereignstudio <sovereignstudiomc@gmail.com>
2026-05-09 18:50:39 +0700

    Auto register blacklist name

--- src/main/java/noctenz/aetherCore/Module.java
+++ src/main/java/noctenz/aetherCore/Module.java
@@ -3,7 +3,6 @@ package noctenz.aetherCore;
 import java.util.List;
 
 public interface Module {
-    String getName();
     List<String> getNames();
     void onEnable(AetherCore plugin);
     void onDisable();
